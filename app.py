53c1d4ba745e9d572b962ce44f3b981c5a5a76e8
siya2799 <pangamsiya2@gmail.com>
2026-03-24 21:23:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="🎧 United States Top 50 Playlist Analytics Dash
 # Set the title of the app
 st.title("United States Top 50 Playlist Performance and Song Popularity Trend Analysis Dashboard")
 # Load the dataset
-df = pd.read_csv(r"C:\Users\User\Desktop\GitHubFiles\US-top50-playlist-analysis\data\Atlantic_United_States.csv")
+df = pd.read_csv(r"https://github.com/siya2799/US-top50-playlist-analysis/blob/main/data/Atlantic_United_States.csv")
 #Convert the 'Date' column to datetime format
 df['date'] = pd.to_datetime(df['date'])
 #Create "primary artist" column by extracting the first artist from the 'artist' column
